306ccc3c1706132c30a09ae09727139a6eb98246
Travon Royal <RoyalTra31@gmail.com>
2016-11-02 15:24:52 -0500

    Golfmain and player

--- Player_Controller.java
+++ Player_Controller.java
@@ -1,40 +1,116 @@
-package golf;
+package golfLeague;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.stream.events.StartDocument;
 
 import javafx.application.Application;
+import javafx.beans.binding.IntegerExpression;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Player_Controller extends golfMain{
+public class Player_Controller extends golfMain {
 
 	@FXML
-	TreeTableView treeView = new TreeTableView();
+	static
+	TreeTableView<Player> treeView = new TreeTableView<>();
 
 	@FXML
-	TreeTableColumn name = new TreeTableColumn();
+	static
+	TreeTableColumn<Player, String> name = new TreeTableColumn<>();
 	
+	@FXML
+	static
+	TreeTableColumn<Player, String> handicap = new TreeTableColumn<>();
 	
-	public void initialize(){
+	@FXML
+	static
+	TreeTableColumn<Player, String> rank = new TreeTableColumn<>();
+	
+	@FXML
+	static
+	TreeTableColumn<Player, String> team = new TreeTableColumn<>();
+
+	@FXML
+	Button start = new Button();
+
+	@FXML
+	AnchorPane scene;
+
+	@SuppressWarnings("unchecked")
+	public static void populateTable() throws ClassNotFoundException, SQLException {
 		
+		 ResultSet ult = golfMain.dataSet("*");
+	        
+	        List<Player> players = new ArrayList<>();
+	        
+	        while(ult.next()){
+	        	players.add(
+	        			new Player(ult.getString(3), ult.getString(4),ult.getString(6), ult.getString(7), ult.getString(8), ult.getString(9)));
+	        }
+	        
+	       
+	        
+			final TreeItem<Player> root = new TreeItem<>();
 		
-		Player player1 = new Player("Travon", "Royal", 73, 76, 1);
-		
-		TreeItem<String> player = new TreeItem<>(player1.getfName());
-		
-		TreeTableColumn<String, String> name = new TreeTableColumn<>("Player");
-		
-		name.setCellValueFactory((javafx.scene.control.TreeTableColumn.CellDataFeatures<String, String> p) -> 
-			new ReadOnlyStringWrapper(p.getValue().getValue()));
-		
-		TreeTableView<String> treeView = new TreeTableView<>(player);
-		
-		treeView.getColumns().add(name);
-		
+			root.setExpanded(true);
+			
+			players.stream().forEach((player) -> {
+				root.getChildren().add(new TreeItem<>(player));
+			});
+			
+			rank = new TreeTableColumn<>("Rank");
+			rank.setPrefWidth(100);
+			rank.setCellValueFactory(
+					(TreeTableColumn.CellDataFeatures<Player, String> p) ->
+					new ReadOnlyStringWrapper(p.getValue().getValue().getRank())
+					);
+			
+			name = new TreeTableColumn<>("Name");
+			name.setPrefWidth(100);
+			name.setCellValueFactory(
+					(TreeTableColumn.CellDataFeatures<Player, String> p) ->
+					new ReadOnlyStringWrapper(p.getValue().getValue().getfName() 
+							+ p.getValue().getValue().getlName())
+					);
+			
+			team = new TreeTableColumn<>("Team");
+			team.setPrefWidth(100);
+			team.setCellValueFactory(
+					(TreeTableColumn.CellDataFeatures<Player, String> p) ->
+					new ReadOnlyStringWrapper(p.getValue().getValue().getTeam())
+					);
+			
+			handicap = new TreeTableColumn<>("Handicap");
+			handicap.setPrefWidth(100);
+			handicap.setCellValueFactory(
+					(TreeTableColumn.CellDataFeatures<Player, String> p) ->
+					new ReadOnlyStringWrapper(p.getValue().getValue().getHandicap())
+					);
+			
+			treeView = new TreeTableView<>(root);
+			treeView.getColumns().setAll(rank, name, team, handicap);
+	        treeView.setPrefWidth(400);
+	        treeView.setShowRoot(false); 
+	
 		
 	}
+		
 }
--- golfMain.java
+++ golfMain.java
@@ -1,227 +1,353 @@
-
 package golfLeague;
 
 import java.io.IOException;
+
 import java.util.HashMap;
+
 import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
+
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+
 import javafx.scene.layout.AnchorPane;
+
 import javafx.scene.layout.BorderPane;
+
 import javafx.stage.Stage;
 
 public class golfMain extends Application {
 
 	private Stage primaryStage;
+
 	private BorderPane rootLayout;
+
 	private AnchorPane login;
+
 	private AnchorPane admin;
+
 	private BorderPane createUser;
+
 	private BorderPane createUser2;
-	
+
 	public String userName = "";
+
 	public String pass = "";
+
 	public String type = "";
+
 	public Map<String, Person> hashMap;
-	
+
+	private AnchorPane player;
 
 	@Override
+
 	public void start(Stage primaryStage) throws IOException {
+
 		this.primaryStage = primaryStage;
+
 		// title
+
 		this.primaryStage.setTitle("Login");
+
 		// load both fxml files
+
 		initializeRootLayout();
+
 		showLogin();
+
 	}
 
 	private void initializeRootLayout() {
+
 		FXMLLoader loader = new FXMLLoader();
+
 		loader.setLocation(golfMain.class.getResource("RootLayout.fxml"));
 
 		try {
+
 			rootLayout = (BorderPane) loader.load();
+
 		} catch (IOException e) {
+
 			e.printStackTrace();
+
 		}
+
 		// set borderpane in scene and stage
+
 		Scene scene = new Scene(rootLayout);
+
 		primaryStage.setScene(scene);
+
 		primaryStage.show();
+
 	}
 
 	private void showLogin() {
 
 		FXMLLoader loader = new FXMLLoader();
+
 		loader.setLocation(golfMain.class.getResource("Login.fxml"));
 
 		try {
+
 			login = (AnchorPane) loader.load();
+
 		} catch (IOException e) {
 
 			e.printStackTrace();
+
 		}
 
 		// Set login into the center of root layout.
+
 		rootLayout.setCenter(login);
+
 	}
 
 	public void showAdmin() {
-		
+
 		FXMLLoader loader = new FXMLLoader();
+
 		loader.setLocation(golfMain.class.getResource("Admin.fxml"));
 
 		try {
+
 			admin = (AnchorPane) loader.load();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+		Stage newStage = new Stage();
+
+		newStage.setTitle("Admin");
+
+		BorderPane adminRoot = new BorderPane();
+
+		Scene scene = new Scene(adminRoot, 700, 500);
+
+		newStage.setScene(scene);
+
+		newStage.show();
+
+		adminRoot.setCenter(admin);
+
+		// make methods for each of the pages to open
+
+	}
+
+	public void showPlayer() throws ClassNotFoundException, SQLException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(golfMain.class.getResource("Player.fxml"));
+
+		try {
+
+			player = (AnchorPane) loader.load();
 		} catch (IOException e) {
 
 			e.printStackTrace();
 		}
+
+		Stage stage = new Stage();
+		stage.setTitle("PlayerPage");
+
+		BorderPane playerRoot = new BorderPane();
+
+		stage.setTitle("PlayerPage");
+        final Scene scene = new Scene(new Group(), 400, 400);
+        Group sceneRoot = (Group)scene.getRoot();  
+        Player_Controller.populateTable();
+		sceneRoot.getChildren().add(Player_Controller.treeView);
 		
-		Stage newStage = new Stage();
-		newStage.setTitle("Admin");
+		stage.setScene(scene);
+		stage.show();
+
 		
-		BorderPane adminRoot = new BorderPane();
-		Scene scene = new Scene(adminRoot, 700, 500);
-		newStage.setScene(scene);
-		
-		newStage.show();
-		
-		adminRoot.setCenter(admin);
-		
-		// make methods for each of the pages to open
 	}
-	
+
 	public void showLeaguePage() {
-		
-		
+
 	}
-	
+
 	public void showCreateUser() {
-		
+
 		FXMLLoader loader = new FXMLLoader();
+
 		loader.setLocation(golfMain.class.getResource("CreateUser.fxml"));
 
 		try {
+
 			createUser = (BorderPane) loader.load();
+
 		} catch (IOException e) {
 
 			e.printStackTrace();
+
 		}
-		
+
 		Stage newStage = new Stage();
+
 		newStage.setTitle("Create New User");
-		
 
 		Scene scene = new Scene(createUser, 400, 400);
+
 		newStage.setScene(scene);
-		
+
 		newStage.show();
-		
-		
+
 	}
-	
-	public void showCreateUser2 (String user, String pass, String type) {
+
+	public void showCreateUser2() {
+
 		
-		userName = user;
-		pass = pass;
-		type = type;
-		
+
 		FXMLLoader loader = new FXMLLoader();
 
 		loader.setLocation(golfMain.class.getResource("CreateUser2.fxml"));
 
 		try {
+
 			createUser2 = (BorderPane) loader.load();
+
 		} catch (IOException e) {
 
 			e.printStackTrace();
+
 		}
-		
+
 		Stage newStage = new Stage();
+
 		newStage.setTitle("Create Account");
-		
 
 		Scene scene = new Scene(createUser2, 400, 400);
+
 		newStage.setScene(scene);
-		
+
 		newStage.show();
-		
-		
+
 	}
-	
-	public void insertPersonAll(String uname, String pword, String fname, String lname, String type, String handicap, String score, String rank, String team ) throws SQLException, ClassNotFoundException{
+
+	public void insertPersonAll(String uname, String pword, String fname, String lname, String type, String handicap,
+			String score, String rank, String team) throws SQLException, ClassNotFoundException {
+
 		Class.forName("com.mysql.jdbc.Driver");
-		
+
 		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/golf_league", "travon",
+
 				"tra");
-		
-		
+
 		String insertString = "insert into person (userName, password, fname,lname, type, handicap, score, rank, team) "
+
 				+ "values (?,?,?,?,?,?,?,?,?)";
+
 		PreparedStatement statement = connection.prepareStatement(insertString);
-		
+
 		statement.setString(1, uname);
+
 		statement.setString(2, pword);
+
 		statement.setString(3, fname);
+
 		statement.setString(4, lname);
+
 		statement.setString(5, type);
+
 		statement.setString(6, handicap);
+
 		statement.setString(7, score);
+
 		statement.setString(8, rank);
+
 		statement.setString(9, team);
-		
+
 		statement.executeUpdate();
-		
-	
+
 		connection.close();
+
 	}
 
-	public void update(String up, String set, String where) throws SQLException, ClassNotFoundException{
-		
-		Class.forName("com.mysql.jdbc.Driver");
-		
-			java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/golf_league", "travon",
-				"tra");
-			
-			String update = "update ? set ? where ?";
-						
-			PreparedStatement statement = connection.prepareStatement(update);
-			
-			statement.setString(1, up);
-			statement.setString(2,  set);
-			statement.setString(3, where);
-			
-			statement.executeUpdate();
-	}
+	public void update(String up, String set, String where) throws SQLException, ClassNotFoundException {
 
-	public String data(String field, String where) throws SQLException, ClassNotFoundException{
-		
 		Class.forName("com.mysql.jdbc.Driver");
-		
+
 		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/golf_league", "travon",
-			"tra");
-		
-		String select = "select "+ field + " from person where " + where;
-					
+
+				"tra");
+
+		String update = "update ? set ? where ?";
+
+		PreparedStatement statement = connection.prepareStatement(update);
+
+		statement.setString(1, up);
+
+		statement.setString(2, set);
+
+		statement.setString(3, where);
+
+		statement.executeUpdate();
+
+	}
+
+	public String data(String field, String where) throws SQLException, ClassNotFoundException {
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/golf_league", "travon",
+
+				"tra");
+
+		String select = "select " + field + " from person where " + where;
+
 		Statement state = connection.createStatement();
-		
+
 		ResultSet ult = state.executeQuery(select);
-		
+
 		ult.next();
+
 		return ult.getString(1);
+
+	}
+
+	public static ResultSet dataSet(String field) throws SQLException, ClassNotFoundException {
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		java.sql.Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/golf_league", "travon",
+
+				"tra");
+
+		String select = "select " + field + " from person";
+
+		Statement state = connection.createStatement();
+
+		ResultSet ult = state.executeQuery(select);
+
+		ult.next();
 		
-		
-		
+
+		return ult;
+
 	}
 	public static void main(String[] args) {
+
 		Map<String, Person> hashMap = new HashMap<>();
+
 		launch(args);
+
 	}
+
 }
